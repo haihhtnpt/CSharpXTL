37f2b0c92cd0397c52507c8be31280795e51fb21
haihhtnpt <haihhtnpt24@gmail.com>
2022-10-07 15:18:13 +0700

    CS02_Method

---
+++ CS02_Method/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace  CS02_Method{
+class Program {
+    public static void Main(string[] args){
+     var a=CS02_Method.tinhtoan.sum(12,23);
+     Console.WriteLine(a);
+     CS02_Method.tinhtoan.noichuoi("hai","Nguyen");
+    }
+
+
+}
+}
---
+++ CS02_Method/tinhtoan.cs
@@ -0,0 +1,12 @@
+namespace CS02_Method{
+public class tinhtoan{
+   public static int sum(int a, int b){
+    return a+b;
+   }
+   public static void noichuoi(string a,string b){
+    string fullname;
+    fullname=a+ " " + b;
+    Console.WriteLine($"Ten la {fullname}");
+   }
+}
+}
